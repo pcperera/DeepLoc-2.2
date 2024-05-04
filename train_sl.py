0896f3f7c5f38ab11df7797f06db4ff736cad65a
Priyanka Perera <pcperera@gmail.com>
2024-05-04 08:41:54 +0530

    Use Accurate as teh default model

--- train_sl.py
+++ train_sl.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "-m","--model", 
-        default="Fast",
+        default="Accurate",
         choices=['Accurate', 'Fast'],
         type=str,
         help="Model to use."
